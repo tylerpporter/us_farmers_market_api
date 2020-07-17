f84e4d3213ae3d9d103246e43a857eed8a0514f1
Tyler Porter <tylerpporter@gmail.com>
2020-07-17 06:31:20 -0600

    Rename ModelGenerator to ModelManager

--- db/csv/model_manager.rb
+++ db/csv/model_manager.rb
@@ -1,11 +1,11 @@
 require_relative 'farmers_market_generator.rb'
 
-class ModelGenerator
+class ModelManager
   class << self
-    def destroy_and_create
+    def destroy_and_create_markets(csv = File.open('db/csv/markets.csv'))
       Product.destroy_all
       Market.destroy_all
-      markets = FarmersMarketGenerator.new
+      markets = FarmersMarketGenerator.new(csv)
       markets.import
     end
   end
