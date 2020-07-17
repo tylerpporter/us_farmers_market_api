71802d456c8c45e076fb7449994e4bcdad2e4975
Tyler Porter <tylerpporter@gmail.com>
2020-07-17 06:32:02 -0600

    Create test for ModelManager

--- spec/tasks/model_generator_spec.rb
+++
@@ -1,9 +0,0 @@
-# require 'rails_helper'
-# require './db/csv/model_generator.rb'
-#
-# describe 'market_generator task' do
-#   it 'should generate markets' do
-#     ModelGenerator.destroy_and_create
-#     expect(Market.all.size).to eq(8812)
-#   end
-# end
