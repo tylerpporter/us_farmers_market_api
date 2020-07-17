c5ebace2e650bec85e8217ae322535469c9d6e61
Colin Alexander <colintalex@gmail.com>
2020-07-17 16:18:11 -0600

    Adds search for single market query and tests

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,5 +4,20 @@ module Types
     def all_markets
       Market.all
     end
+
+    # Save for future use to search markets by products, etc.
+    # 
+    # field :all_markets, [Types::MarketType], null: false
+    # def all_markets
+    #   Market.all
+    # end
+
+    field :market, Types::MarketType, null: false do
+      argument :id, Integer, required: true
+    end
+    def market(id)
+      Market.find(id[:id])
+    end
+    
   end
 end
