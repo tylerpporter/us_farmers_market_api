aabe1c7e9797a4b8202f254c1d5f35866efcd5af
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-18 15:26:36 -0600

    Revert changes

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,7 @@ require "action_controller/railtie"
 # require "action_mailer/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
-
-if Rails.env.development?
-  require "sprockets/railtie"
-end
-
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
