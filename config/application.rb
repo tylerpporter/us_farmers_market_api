043d0e4b069e7f8a488c6f365fbbb92190ac3b42
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-18 15:45:02 -0600

    Remove assets for production

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,5 @@ module UsFarmersMarketsApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.assets.initialize_on_precompile = false
   end
 end
