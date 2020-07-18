fe5e341daab6ebfdd2851460b03789813efeae63
Zach Holcomb <zachholcombmusic@gmail.com>
2020-07-18 11:59:28 -0600

    Add routes for graphql interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  post "/", to: "graphql#execute"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/', to: 'graphql#execute'
+  # graphQL IDE
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+    post "/graphql", to: "graphql#execute"
+  end
+
+  
 end
