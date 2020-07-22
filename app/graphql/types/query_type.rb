ad9e552e01281f4c7657b4cf7379e53c1df61815
Tyler Porter <tylerpporter@gmail.com>
2020-07-22 12:20:09 -0600

    Update marketsByLocation query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -13,13 +13,18 @@ module Types
       Market.find(id)
     end
     # Return markets by location
-    field :markets_by_location, [Types::MarketType], null: false do
+    field :markets_by_location, Types::ReturnType, null: false do
       argument :lat, Float, required: true
       argument :lng, Float, required: true
       argument :radius, Integer, required: true
     end
     def markets_by_location(lat:, lng:, radius:)
-      Market.near([lat, lng], radius)
+      markets = Market.near([lat, lng], radius)
+      location_object = Geocoder.search([lat, lng]).first
+      location = location_object.city + ', ' + location_object.state
+      { markets: markets,
+        location: location
+      }
     end
   end
 end
