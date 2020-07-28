a7497c15c3b2cca0cfe05f1c3870ffb13a3f18c0
Tyler Porter <tylerpporter@gmail.com>
2020-07-28 14:34:55 -0600

    Add descriptions for return types

--- app/graphql/types/return_type.rb
+++ app/graphql/types/return_type.rb
@@ -1,8 +1,8 @@
 module Types
   class ReturnType < Types::BaseObject
     field :markets, [Types::MarketType], null: false
-    field :location, String, null: true
-    field :latitude, Float, hash_key: 'latitude', null: true
-    field :longitude, Float, hash_key: 'longitude', null: true
+    field :location, String, "The city, state for the request lat, lng.", null: true
+    field :latitude, Float, "The latitude for the request city, state.", hash_key: 'latitude', null: true
+    field :longitude, Float, "The longitude for the request city, state.", hash_key: 'longitude', null: true
   end
 end
